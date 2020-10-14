83a2d1ae0ee425cfc75c99cb38d0eb23ec10861e
Sarvesh Hiwase <69983214+SarveshtheAbstractor@users.noreply.github.com>
2020-10-14 22:59:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     
     
     //Check if player won the game
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 50){
         document.querySelector('#name-'+activePlayer).textContent = "Winner!";
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
